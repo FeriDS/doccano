c2a3404fd2b0b0f0359204b2902f154fe51825de
dluz1991 <a65873@ualg.pt>
2025-06-07 18:13:32 +0100

    reverting

--- backend/perspective/permissions.py
+++
@@ -1,4 +0,0 @@
-from rest_framework.permissions import BasePermission
-class IsProjectAdmin(BasePermission):
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.admin
